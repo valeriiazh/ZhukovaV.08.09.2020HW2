62a77a32462b236f75a49188d441f94cd97d1699
leraz <leraz@PSIs-MacBook-Air-10.local>
2020-09-08 23:10:02 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+thislist = ["Kyiv", "Tokyo", "Madrid", "Busan"]
+print(thislist)
+capital = input('Enter your option: ').capitalize()
+thislist.append(capital)
+print(thislist)
+thislist.pop()
+print(thislist)
